e390744a10cd9f9e0469ca08ef4057746bd3e380
Ilya Akkuzin <gr3yknigh1@gmail.com>
2023-03-30 19:33:54 +0300

    Verify config file

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,46 @@
+#include <filesystem>
 #include <iostream>
+#include <string>
+#include <vector>
+#include <yaml-cpp/yaml.h>
 
+const char *REQUIRED_CONFIG_FIELDS[] = {"files", "host", "target"};
 
-int main(void)
+int main(int argc, const char **argv)
 {
-    std::cout << "Hi hi there\n";
-    return 0;
+    if (argc < 2)
+    {
+        std::cout << "usage: async-http-downloader <path-to-config.yaml>\n";
+        return EXIT_SUCCESS;
+    }
+
+    std::filesystem::path configPath(argv[1]);
+
+    if (!std::filesystem::exists(configPath))
+    {
+        std::fprintf(stderr, "ERROR: given file path doesn't exitsts: '%s'\n",
+                     configPath.c_str());
+        return EXIT_FAILURE;
+    }
+
+    YAML::Node configYaml = YAML::LoadFile(configPath);
+
+    // NOTE: Added in order to exclude invalid yaml files
+    if (!configYaml.IsMap())
+    {
+        std::fprintf(stderr, "ERROR: invalid config\n");
+        return EXIT_FAILURE;
+    }
+
+    for (const char *field : REQUIRED_CONFIG_FIELDS)
+    {
+        if (!configYaml[field])
+        {
+            std::fprintf(stderr, "ERROR: missing required config field: '%s'",
+                         field);
+            return EXIT_FAILURE;
+        }
+    }
+
+    return EXIT_SUCCESS;
 }
