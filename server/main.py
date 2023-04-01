beb8881fb717bffacbe137c538182918a62aa04d
Ilya Akkuzin <gr3yknigh1@gmail.com>
2023-04-01 12:22:52 +0300

    Make server return file during api respond

--- server/main.py
+++ server/main.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 from __future__ import annotations
-from flask import Flask
+from flask import Flask, send_file
 from flask_restful import Api
+from os.path import join
 
 app = Flask(__name__)
 api = Api(app)
+
+@app.route("/api/files/<string:filename>")
+def return_file(filename):
+    # TODO: Extract file path routing
+    return send_file(join("data", filename))
 
 def main() -> int:
     app.run(debug=True)
