513c114ef038b02fbf403b50ebc52d7837cf45e0
Ilya Akkuzin <gr3yknigh1@gmail.com>
2023-04-04 18:37:55 +0300

    Validate yaml fields

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <cassert>
+#include <cstdint>
 #include <filesystem>
 #include <fstream>
 #include <future>
@@ -10,6 +11,23 @@
 #include <HTTPRequest.hpp>
 #include <bit7z/bit7z.hpp>
 #include <yaml-cpp/yaml.h>
+
+const std::vector<const char *> FindMissingFields(
+    YAML::Node node, const std::vector<const char *> requiredFields)
+{
+    // NOTE: Maybe reserve memory?
+    std::vector<const char *> missingFields;
+
+    for (const char *field : requiredFields)
+    {
+        if (!node[field])
+        {
+            missingFields.push_back(field);
+        }
+    }
+
+    return missingFields;
+}
 
 class Action
 {
@@ -103,15 +121,15 @@ private:
 const char *CONFIG_HOST_FIELD = "host";
 const char *CONFIG_TARGET_FIELD = "target";
 const char *CONFIG_FILES_FIELD = "files";
-const char *REQUIRED_CONFIG_FIELDS[] = {CONFIG_HOST_FIELD, CONFIG_TARGET_FIELD,
-                                        CONFIG_FILES_FIELD};
+const std::vector<const char *> REQUIRED_CONFIG_FIELDS = {
+    CONFIG_HOST_FIELD, CONFIG_TARGET_FIELD, CONFIG_FILES_FIELD};
 
 const char *FILE_NAME_FIELD = "name";
 const char *FILE_FILE_FIELD = "file";
 const char *FILE_ACTIONS_FIELD = "actions";
 const char *FILE_DEPENDENCIES_FIELD = "dependencies";
-const char *REQUIRED_FILE_FIELDS[] = {FILE_NAME_FIELD, FILE_FILE_FIELD,
-                                      FILE_ACTIONS_FIELD};
+const std::vector<const char *> REQUIRED_FILE_FIELDS = {
+    FILE_NAME_FIELD, FILE_FILE_FIELD, FILE_ACTIONS_FIELD};
 
 int main(int argc, const char **argv)
 {
@@ -139,39 +157,73 @@ int main(int argc, const char **argv)
         return EXIT_FAILURE;
     }
 
-    for (const char *field : REQUIRED_CONFIG_FIELDS)
+    const std::vector<const char *> missingFields =
+        FindMissingFields(configYaml, REQUIRED_CONFIG_FIELDS);
+    if (missingFields.size() != 0)
     {
-        if (!configYaml[field])
+        std::fprintf(stderr, "ERROR: missing required config fields: ");
+        for (const auto &missingField : missingFields)
         {
-            std::fprintf(stderr, "ERROR: missing required config field: '%s'\n",
-                         field);
-            return EXIT_FAILURE;
+            std::fprintf(stderr, "'%s' ", missingField);
         }
+        std::fprintf(stderr, "\n");
+        return EXIT_FAILURE;
     }
 
     if (!configYaml[CONFIG_FILES_FIELD].IsSequence())
     {
-        std::fprintf(stderr, "ERROR: 'files' field isn't sequence\n");
+        std::fprintf(stderr, "ERROR: 'files' field isn't a sequence\n");
         return EXIT_FAILURE;
     }
 
-    for (const auto fileYaml : configYaml[CONFIG_FILES_FIELD])
+    const std::string host = configYaml[CONFIG_HOST_FIELD].as<std::string>();
+    const std::string target =
+        configYaml[CONFIG_TARGET_FIELD].as<std::string>();
+
+    std::vector<FileTask> fileTasks;
+    fileTasks.reserve(configYaml[CONFIG_FILES_FIELD].size());
+
+    for (uint64_t i = 0; i < configYaml[CONFIG_FILES_FIELD].size(); ++i)
     {
-        for (const auto field : REQUIRED_FILE_FIELDS)
+        const YAML::Node fileYaml = configYaml[CONFIG_FILES_FIELD][i];
+        const std::vector<const char *> missingFields =
+            FindMissingFields(fileYaml, REQUIRED_FILE_FIELDS);
+        if (missingFields.size() != 0)
         {
-            if (!fileYaml[field])
+            std::fprintf(
+                stderr,
+                "ERROR: missing required file fields at index %ld: ", i);
+            for (const auto &missingField : missingFields)
             {
-                std::fprintf(stderr,
-                             "ERROR: missing required file field: '%s'\n",
-                             field);
-                return EXIT_FAILURE;
+                std::fprintf(stderr, "'%s' ", missingField);
             }
+            std::fprintf(stderr, "\n");
+            return EXIT_FAILURE;
         }
 
-        std::cout << fileYaml << '\n';
+        FileTask fileTask;
+        fileTask.name = fileYaml[FILE_NAME_FIELD].as<std::string>();
+        fileTask.file = fileYaml[FILE_FILE_FIELD].as<std::string>();
+        fileTask.actions.reserve(fileYaml[FILE_ACTIONS_FIELD].size());
 
-        for (const auto action : fileYaml[FILE_ACTIONS_FIELD])
+        for (const auto actionYaml : fileYaml[FILE_ACTIONS_FIELD])
         {
+            const std::string actionString = actionYaml.as<std::string>();
+            if (actionString == "download")
+            {
+                // TODO: Add option for working directory
+                fileTask.actions.emplace_back(DownloadAction(
+                    host + target + fileTask.file, fileTask.file));
+            }
+            else if (actionString == "unpack")
+            {
+                // TODO: Add option for unpack directory
+                fileTask.actions.emplace_back(UnpackAction(fileTask.file, "."));
+            }
+            else
+            {
+                // TODO: Throw error
+            }
         }
 
         if (fileYaml[FILE_DEPENDENCIES_FIELD])
@@ -183,12 +235,6 @@ int main(int argc, const char **argv)
             }
         }
     }
-
-    const std::string host = configYaml[CONFIG_HOST_FIELD].as<std::string>();
-    const std::string target =
-        configYaml[CONFIG_TARGET_FIELD].as<std::string>();
-
-    const std::string targetRequestUrl = host + target;
 
     return EXIT_SUCCESS;
 }
