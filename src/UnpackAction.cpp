43b246af31f4a9de85b06263a9b619b47f00930f
Ilya Akkuzin <gr3yknigh1@gmail.com>
2023-04-04 21:55:50 +0300

    Check for archive's path existens in `UnpackAction`

--- src/UnpackAction.cpp
+++ src/UnpackAction.cpp
@@ -1,4 +1,7 @@
 #include "ahd/UnpackAction.hpp"
+#include <cstdio>
+#include <cstdlib>
+#include <filesystem>
 
 UnpackAction::UnpackAction(const std::filesystem::path &archivePath,
                            const std::filesystem::path &destanationPath)
@@ -9,5 +12,14 @@ UnpackAction::UnpackAction(const std::filesystem::path &archivePath,
 
 void UnpackAction::Execute(void) const
 {
+    if (!std::filesystem::exists(m_ArchivePath))
+    {
+        std::fprintf(stderr,
+                     "ERROR: During `unpack` action execution: '%s' archive "
+                     "path doesn't exist\n",
+                     m_ArchivePath.c_str());
+        std::exit(EXIT_FAILURE);
+    }
+
     s_7zExtractor->extract(m_ArchivePath, m_DestanationPath);
 }
