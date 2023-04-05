74088dc913ef0e87f7cb7f7f0b85e1842ae0dfae
Ilya Akkuzin <gr3yknigh1@gmail.com>
2023-04-05 18:37:01 +0300

    Move all logic of config reading and validating to `ConfigReader`

--- src/main.cpp
+++ src/main.cpp
@@ -18,212 +18,240 @@
 #include "ahd/TaskRunner.hpp"
 #include "ahd/UnpackAction.hpp"
 
-const char *CONFIG_HOST_FIELD = "host";
-const char *CONFIG_TARGET_FIELD = "target";
-const char *CONFIG_FILES_FIELD = "files";
-const std::vector<const char *> REQUIRED_CONFIG_FIELDS = {
-    CONFIG_HOST_FIELD, CONFIG_TARGET_FIELD, CONFIG_FILES_FIELD};
-
-const char *FILE_NAME_FIELD = "name";
-const char *FILE_FILE_FIELD = "file";
-const char *FILE_ACTIONS_FIELD = "actions";
-const char *FILE_DEPENDENCIES_FIELD = "dependencies";
-const std::vector<const char *> REQUIRED_FILE_FIELDS = {
-    FILE_NAME_FIELD, FILE_FILE_FIELD, FILE_ACTIONS_FIELD};
-
 using TaskMap = std::unordered_map<std::string, std::shared_ptr<Task>>;
 
-const std::vector<const char *> FindMissingFields(
-    YAML::Node node, const std::vector<const char *> requiredFields)
+class ConfigReader
 {
-    // NOTE: Maybe reserve memory?
-    std::vector<const char *> missingFields;
-
-    for (const char *field : requiredFields)
+public:
+    virtual ~ConfigReader()
     {
-        if (!node[field])
-        {
-            missingFields.push_back(field);
-        }
     }
 
-    return missingFields;
-}
-
-void ValidateDependencies(const TaskMap &taskMap)
-{
-    for (const auto &[name, task] : taskMap)
+    virtual TaskMap Read(const std::filesystem::path configPath)
     {
-        for (const std::string &dependency : task->dependencies)
+        throw std::exception();
+    }
+
+protected:
+    inline static const char *s_ConfigHostField = "host";
+    inline static const char *s_ConfigTargetField = "target";
+    inline static const char *s_ConfigFilesField = "files";
+    inline static const std::vector<const char *> s_RequiredConfigFields = {
+        s_ConfigHostField, s_ConfigTargetField, s_ConfigFilesField};
+
+    inline static const char *s_FileNameField = "name";
+    inline static const char *s_FileFileField = "file";
+    inline static const char *s_FileActionsField = "actions";
+    inline static const char *s_FileDependenciesField = "dependencies";
+    inline static const std::vector<const char *> s_RequiredFileFields = {
+        s_FileNameField, s_FileFileField, s_FileActionsField};
+};
+
+class YamlConfigReader : public ConfigReader
+{
+public:
+    TaskMap Read(const std::filesystem::path configPath) override
+    {
+        const YAML::Node configYaml = YAML::LoadFile(configPath);
+        ValidateConfigYaml(configYaml);
+
+        const std::string host =
+            configYaml[s_ConfigHostField].as<std::string>();
+        const std::string target =
+            configYaml[s_ConfigTargetField].as<std::string>();
+
+        TaskMap taskMap = {};
+        taskMap.reserve(configYaml[s_ConfigFilesField].size());
+
+        for (uint64_t i = 0; i < configYaml[s_ConfigFilesField].size(); ++i)
         {
-            // NOTE: Searching non-existing deps
-            const auto &search = taskMap.find(dependency);
-            if (search == taskMap.end())
+            const YAML::Node fileYaml = configYaml[s_ConfigFilesField][i];
+            ValidateFileYaml(i, fileYaml);
+
+            std::shared_ptr<Task> task = std::make_shared<Task>();
+            task->file = fileYaml[s_FileFileField].as<std::string>();
+            task->actions = DispatchActionsYaml(i, host, target, task,
+                                                fileYaml[s_FileActionsField]);
+
+            if (fileYaml[s_FileDependenciesField])
             {
-                std::fprintf(stderr,
-                             "ERROR: Can't find dependency '%s' that '%s' task "
-                             "requires\n",
-                             dependency.c_str(), name.c_str());
-                std::exit(EXIT_FAILURE);
-                return;
+                task->dependencies =
+                    DispatchDependenciesYaml(fileYaml[s_FileDependenciesField]);
             }
 
-            // NOTE: Can't depend on self
-            if (search->first == name)
+            taskMap[fileYaml[s_FileNameField].as<std::string>()] = task;
+        }
+
+        ValidateDependencies(taskMap);
+
+        return taskMap;
+    }
+
+private:
+    const std::vector<const char *> FindMissingFields(
+        const YAML::Node &node, const std::vector<const char *> &requiredFields)
+    {
+        std::vector<const char *> missingFields;
+        missingFields.reserve(requiredFields.size());
+
+        for (const char *field : requiredFields)
+        {
+            if (!node[field])
             {
-                std::fprintf(stderr, "ERROR: Can't depend on self '%s'\n",
-                             name.c_str());
-                std::exit(EXIT_FAILURE);
-                return;
-            }
-
-            // NOTE: Searching cirqle deps
-            const std::shared_ptr<Task> dependencyTask = search->second;
-            const auto dependencyTaskDeps = dependencyTask->dependencies;
-            if (std::find(dependencyTaskDeps.begin(), dependencyTaskDeps.end(),
-                          name) != dependencyTaskDeps.end())
-            {
-                std::fprintf(stderr,
-                             "ERROR: Found cirqle dependency between '%s' and "
-                             "'%s' tasks requires\n",
-                             dependency.c_str(), name.c_str());
-                std::exit(EXIT_FAILURE);
-                return;
+                missingFields.emplace_back(field);
             }
         }
-    }
-}
 
-void ValidateConfigYaml(const YAML::Node &configYaml)
-{
-    // NOTE: Added in order to exclude invalid yaml files
-    if (!configYaml.IsMap())
-    {
-        throw std::invalid_argument("Config isn't a map of fields\n");
-        return;
+        missingFields.shrink_to_fit();
+        return missingFields;
     }
 
-    const auto missingFields =
-        FindMissingFields(configYaml, REQUIRED_CONFIG_FIELDS);
-    if (missingFields.size() != 0)
+    void ValidateDependency(const std::string &name, const TaskMap &taskMap,
+                            const std::string &dependency)
     {
-        std::string errorMessage = "Missing required config fields: ";
-        for (const char *field : missingFields)
-        {
-            errorMessage += field;
-        }
-        throw std::invalid_argument(errorMessage);
-        return;
-    }
+        const auto &dependencySearch = taskMap.find(dependency);
 
-    if (!configYaml[CONFIG_FILES_FIELD].IsSequence())
-    {
-        throw std::invalid_argument("'files' field must be a sequence");
-        return;
-    }
-}
-
-void ValidateFileYaml(uint64_t index, const YAML::Node &fileYaml)
-{
-    const std::vector<const char *> missingFields =
-        FindMissingFields(fileYaml, REQUIRED_FILE_FIELDS);
-    if (!missingFields.empty())
-    {
-        std::ostringstream errorMessage;
-        errorMessage << "Missing required file fields at index " << index
-                     << ": ";
-        for (const char *missingField : missingFields)
-        {
-            errorMessage << "'" << missingField << "' ";
-        }
-        errorMessage << '\n';
-
-        throw std::invalid_argument(errorMessage.str());
-    }
-}
-
-std::vector<Action *> DispatchActionsYaml(uint64_t index,
-                                          const std::string &host,
-                                          const std::string &target,
-                                          std::shared_ptr<Task> task,
-                                          const YAML::Node actionsYaml)
-{
-    std::vector<Action *> actions;
-    actions.reserve(actionsYaml.size());
-
-    for (const YAML::Node actionYaml : actionsYaml)
-    {
-        const std::string actionString = actionYaml.as<std::string>();
-
-        if (actionString == "download")
-        {
-            // TODO: Add option for working directory
-            actions.push_back(
-                new DownloadAction(host + target + task->file, task->file));
-        }
-        else if (actionString == "unpack")
-        {
-            // TODO: Add option for unpack directory
-            actions.push_back(new UnpackAction(task->file, "."));
-        }
-        else
+        if (dependencySearch == taskMap.end())
         {
             std::ostringstream errorMessage;
-            errorMessage << "Unknown action at " << index << ": '"
-                         << actionString << "'";
+            errorMessage << "Can't find dependency '" << dependency
+                         << "' that '" << name << "' requires";
+            throw std::invalid_argument(errorMessage.str());
+        }
+
+        if (dependencySearch->first == name)
+        {
+            std::ostringstream errorMessage;
+            errorMessage << "Can't depend on self '" << name << "'";
+            throw std::invalid_argument(errorMessage.str());
+        }
+
+        const std::shared_ptr<Task> dependencyTask = dependencySearch->second;
+        const std::vector<std::string> dependencyTaskDependencies =
+            dependencyTask->dependencies;
+
+        if (std::find(dependencyTaskDependencies.begin(),
+                      dependencyTaskDependencies.end(),
+                      name) != dependencyTaskDependencies.end())
+        {
+            std::ostringstream errorMessage;
+            errorMessage << "Found cirqle dependency between '" << name
+                         << "' and '" << dependencySearch->first << "' tasks";
             throw std::invalid_argument(errorMessage.str());
         }
     }
 
-    return actions;
-}
-
-std::vector<std::string> DispatchDependenciesYaml(
-    const YAML::Node dependenciesYaml)
-{
-    std::vector<std::string> dependencies;
-    dependencies.reserve(dependenciesYaml.size());
-
-    for (const YAML::Node dependencyYaml : dependenciesYaml)
+    void ValidateDependencies(const TaskMap &taskMap)
     {
-        dependencies.emplace_back(dependencyYaml.as<std::string>());
+        for (const auto &[name, task] : taskMap)
+        {
+            for (const std::string &dependency : task->dependencies)
+            {
+                ValidateDependency(name, taskMap, dependency);
+            }
+        }
     }
 
-    return dependencies;
-}
-
-TaskMap ReadConfig(const std::filesystem::path &configPath)
-{
-    const YAML::Node configYaml = YAML::LoadFile(configPath);
-    ValidateConfigYaml(configYaml);
-
-    const std::string host = configYaml[CONFIG_HOST_FIELD].as<std::string>();
-    const std::string target =
-        configYaml[CONFIG_TARGET_FIELD].as<std::string>();
-
-    TaskMap taskMap = {};
-    taskMap.reserve(configYaml[CONFIG_FILES_FIELD].size());
-
-    for (uint64_t i = 0; i < configYaml[CONFIG_FILES_FIELD].size(); ++i)
+    void ValidateConfigYaml(const YAML::Node &configYaml)
     {
-        const YAML::Node fileYaml = configYaml[CONFIG_FILES_FIELD][i];
-        ValidateFileYaml(i, fileYaml);
-
-        std::shared_ptr<Task> task = std::make_shared<Task>();
-        task->file = fileYaml[FILE_FILE_FIELD].as<std::string>();
-        task->actions = DispatchActionsYaml(i, host, target, task,
-                                            fileYaml[FILE_ACTIONS_FIELD]);
-
-        if (fileYaml[FILE_DEPENDENCIES_FIELD])
+        if (!configYaml.IsMap())
         {
-            task->dependencies =
-                DispatchDependenciesYaml(fileYaml[FILE_DEPENDENCIES_FIELD]);
+            throw std::invalid_argument("Config isn't a map of fields\n");
         }
 
-        taskMap[fileYaml[FILE_NAME_FIELD].as<std::string>()] = task;
+        const auto missingFields =
+            FindMissingFields(configYaml, s_RequiredConfigFields);
+        if (!missingFields.empty())
+        {
+            std::ostringstream errorMessage;
+            errorMessage << "Missing required config fields: ";
+            for (const char *field : missingFields)
+            {
+                errorMessage << field << " ";
+            }
+            throw std::invalid_argument(errorMessage.str());
+        }
+
+        if (!configYaml[s_ConfigFilesField].IsSequence())
+        {
+            throw std::invalid_argument("'files' field must be a sequence");
+        }
     }
 
-    return taskMap;
+    void ValidateFileYaml(uint64_t index, const YAML::Node &fileYaml)
+    {
+        const std::vector<const char *> missingFields =
+            FindMissingFields(fileYaml, s_RequiredFileFields);
+        if (!missingFields.empty())
+        {
+            std::ostringstream errorMessage;
+            errorMessage << "Missing required file fields at index " << index
+                         << ": ";
+            for (const char *missingField : missingFields)
+            {
+                errorMessage << "'" << missingField << "' ";
+            }
+
+            throw std::invalid_argument(errorMessage.str());
+        }
+    }
+
+    std::vector<Action *> DispatchActionsYaml(uint64_t index,
+                                              const std::string &host,
+                                              const std::string &target,
+                                              std::shared_ptr<Task> task,
+                                              const YAML::Node actionsYaml)
+    {
+        std::vector<Action *> actions;
+        actions.reserve(actionsYaml.size());
+
+        for (const YAML::Node actionYaml : actionsYaml)
+        {
+            const std::string actionString = actionYaml.as<std::string>();
+
+            if (actionString == "download")
+            {
+                // TODO: Add option for working directory
+                actions.push_back(
+                    new DownloadAction(host + target + task->file, task->file));
+            }
+            else if (actionString == "unpack")
+            {
+                // TODO: Add option for unpack directory
+                actions.push_back(new UnpackAction(task->file, "."));
+            }
+            else
+            {
+                std::ostringstream errorMessage;
+                errorMessage << "Unknown action at index " << index << ": '"
+                             << actionString << "'";
+                throw std::invalid_argument(errorMessage.str());
+            }
+        }
+
+        return actions;
+    }
+
+    std::vector<std::string> DispatchDependenciesYaml(
+        const YAML::Node dependenciesYaml)
+    {
+        std::vector<std::string> dependencies;
+        dependencies.reserve(dependenciesYaml.size());
+
+        for (const YAML::Node dependencyYaml : dependenciesYaml)
+        {
+            dependencies.emplace_back(dependencyYaml.as<std::string>());
+        }
+
+        return dependencies;
+    }
+};
+
+const std::unique_ptr<ConfigReader> DispatchConfigType(
+    const std::filesystem::path configPath)
+{
+    // TODO: If nessesery, add config dispatching logic
+    return std::make_unique<YamlConfigReader>();
 }
 
 void PrintUsage(void)
@@ -254,10 +282,13 @@ int main(int argc, const char **argv)
         return EXIT_FAILURE;
     }
 
-    TaskMap taskMap = ReadConfig(configPath);
-    ValidateDependencies(taskMap);
+    const std::unique_ptr<ConfigReader> configReader =
+        DispatchConfigType(configPath);
+
+    TaskMap taskMap = configReader->Read(configPath);
 
     TaskRunner runner(taskMap);
+
     runner.Run();
 
     return EXIT_SUCCESS;
